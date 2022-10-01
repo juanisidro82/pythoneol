c5c9620a356ce3679588c70e685f9401a8206ea3
juanisidro82 <juanisidro82@yahoo.com.mx>
2022-10-01 05:38:08 -0500

    make base image for compiling python version 1.0.1, 1.1, 1.2, 1.3, 1.4

--- 1.0.1/compile-python-1.0.1.sh
+++ 1.0.1/compile-python-1.0.1.sh
@@ -1,6 +1,3 @@
-apt-get update
-apt-get -y install make libc6-dev libreadline4-dev libgdbmg1-dev gcc
-
 tar -xvvzf python1.0.1.tar.gz
 cd python-1.0.1
 sed -e "s/#dbm dbmmodule.o/dbm dbmmodule.o/g" Modules/Setup.in > Modules/Setup2.in
@@ -10,7 +7,7 @@ rm Modules/dbmmodule.c
 mv Modules/Setup2.in Modules/Setup.in
 mv Modules/dbmmodule2.c Modules/dbmmodule.c
 
-./configure --with-readline="/python-1.0.1/readline" --with-libc=-lgdbm
+./configure --with-readline="/usr/include/readline" --with-libc=-lgdbm
 make
 make install
 make libinstall
